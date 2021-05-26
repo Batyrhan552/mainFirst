4c2a6ceabd0a3cd89e63bfa9fd85ed7f6163555c
Djailov Batyrhan <batyrhan552@gmail.com>
2021-05-26 10:34:39 +0300

    Firstcommit

---
+++ Main.java
@@ -0,0 +1,23 @@
+//String dd = String.valueOf(day);
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		arr[5];
+		int count = 0;			
+		for(int i = 0; i < arr.length; i++){
+			count += arr[i];
+			
+		}
+		double avg = count / 5;
+		
+		
+		
+		
+		
+		
+	}
+}
